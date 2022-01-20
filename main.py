b8852be855661c6b67e51672d8bf2f58c6ad84f1
Jase Smith <jase.smith@outlook.com>
2022-01-20 17:51:51 +1000

    Replaced main.py boilerplate with flask boilerplate

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python3
 """
-Module Docstring
+Documentation
 """
 
 __author__ = "Jase Smith"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+import os
 
-def main():
-   """ Main entry point of the app """
-   print("hello world")
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+
+def create_app(config=None):
+   app = Flask(__name__)
+
+   # See http://flask.pocoo.org/docs/latest/config
+   app.config.update(dict(DEBUG=True))
+   app.config.update(config or {})
+   # Setup cors headers to allow all domains
+   # https://flask-cors.readthedocs.io/en/latest/
+   CORS(app)
+
+   # Definition of the routes. Put them into their own file. See also
+   # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
+   @app.route("/")
+   def hello_world():
+      return "Hello World"
+
+   @app.route("/foo/<someId>")
+   def foo_url_arg(someId):
+      return jsonify({"echo": someId})
+
+   return app
 
 if __name__ == "__main__":
-   """ This is executed when run from the command line """
-   main()
+   port= int(os.environ.get("PORT", 8000))
+   app = create_app()
+   app.run(host="0.0.0.0", port=port
