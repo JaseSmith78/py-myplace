f5dbefb5256021ad7b888f920b281c47100d1f73
Jason Smith <jason.smith@qca.org.au>
2023-02-03 14:23:51 +1000

    debug

--- main.py
+++ main.py
@@ -100,6 +100,7 @@ def create_app(config=None):
       elif ACValue == 0:
          urlString += '{"ac1":{"zones":{"z' + ACZone + '":{"state":"closed"}}}'
          requests.get(url = urlString)
+      print(urlString)
       return "ok"
 
    @app.route("/zone/<ACZone>/targetTemperature/<ACValue>")
